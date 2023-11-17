b781f23fc864bd9e8e96a75db2925d7622e610e3
fsquaglia <fernandosquaglia@gmail.com>
2023-11-17 15:16:21 -0300

    form alta de plato + cloudinary

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -1,19 +1,15 @@
-
 import { Route, Routes } from "react-router-dom";
-import Home from '../Views/Home/Home'
-
+import Home from "../Views/Home/Home";
+import FormMenu from "../Views/FormMenu/FormMenu";
 
 /*ROUTER PRINCIPAL DE LA APLICACIÓN */
 export const AppRouter = () => {
   return (
-
-
     <Routes>
-
       <Route path="/home" element={<Home />} />
-      {/* <Route path="/form" element={<FormPage />} /> */}
+      <Route path="/form" element={<FormMenu />} />
       {/* <Route path="/menu" element={<MenuPage />} /> */}
       {/*Añade más rutas aqui...  */}
     </Routes>
-  )
-}
+  );
+};
