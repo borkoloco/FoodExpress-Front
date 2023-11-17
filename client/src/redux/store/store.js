aa2f41bad9301ea335ac8ca64c695c9105cdbbd5
Jorge Luciano <jorgelss912345@gmail.com>
2023-11-17 08:59:32 -0500

    Corregido inconveniente con Redux-thunk

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducer/reducer';
+import { createStore, applyMiddleware,compose } from 'redux';
 import thunkMiddleware from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension';
 
+import rootReducer from '../reducer/reducer';
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(
-  rootReducer,
-  applyMiddleware(thunkMiddleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    rootReducer,
+    composeWithDevTools(composeEnhancer(applyMiddleware(thunkMiddleware))),
+    
 );
 
 export default store;
